cfe6249087e0bc7fe9425344b9e3c9e8e27237f1
CDA <10048477+devstoriesplayground@users.noreply.github.com>
2022-11-24 17:20:43 +0800

    feat: add variables

---
+++ variables.py
@@ -0,0 +1,19 @@
+# Variables are containers for storing data values.
+topic = "Python"
+channel_name = "DevStories Playground"
+sub_topic = "Variables"
+
+print("Hello! " + channel_name + ".")
+print("This course is about " + topic + ".")
+print("We are here to learn about " + sub_topic + ".")
+
+print("**********************************")
+x = 7
+name = "Devs"
+
+print(f"Hello, my name is {name} and I'm {x} years old.")
+
+print("**********************************")
+
+
+
