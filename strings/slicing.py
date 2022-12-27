c11e5e375fb286b4232703f83a9483a6138b678f
CDA <10048477+devstoriesplayground@users.noreply.github.com>
2022-12-27 15:30:01 +0800

    feat: add string slicing

---
+++ strings/slicing.py
@@ -0,0 +1,12 @@
+# You can return a range of characters by using the slice syntax.
+
+# Specify the start index and the end index, separated by a colon, to return a part of the string.
+
+# Slice From the Start
+b = "Hello, Coders!"
+print(b[:5])
+
+
+# Slice To the End
+b = "Hello, Coders!"
+print(b[2:])
