7741d5dd09b7af9741f04396e021e2a5d93b43ae
devstoriesplayground <10048477+devstoriesplayground@users.noreply.github.com>
2023-05-07 10:50:40 +0000

    feat: add module and packages

---
+++ module_and_packages.py
@@ -0,0 +1,7 @@
+# import module with following command
+import random 
+colors = ["yellow","red","blue"]
+
+# the shuffle() method takes a sequence, like a list, and reorganize the order of the items.
+random.shuffle(colors)
+print(colors)
