e067a297d26df0f40d3aa18b290c3f94d39065da
devstoriesplayground <10048477+devstoriesplayground@users.noreply.github.com>
2023-08-13 12:35:07 +0000

    feat: add python comment

---
+++ comments.py
@@ -0,0 +1,24 @@
+# python block comment explains the code follows it. To make single-line comments in Python, prepend each line with a hash (#).
+
+# age must be 18
+age = 18
+
+
+# python inline comment placed in the same line of the string. It was same with block comment, an inline comment starts with single hash (#) followed by space and string. 
+
+salary = 10000
+salary_increase = 0.05   # salary increases to 5 percent
+
+
+# python docstring is a string literal that you put in the first lines in a code block.
+
+'''This is a multi-line comment with docstrings
+
+print("Hello world")
+print("Hello universe")
+print("Hello everyone")
+'''
+
+
+print(f'Im {age} years old')
+print((salary * salary_increase) + salary)
