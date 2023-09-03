910e7ba98177814dbf1f1bb4e04da47f2830e32c
devstoriesplayground <10048477+devstoriesplayground@users.noreply.github.com>
2023-09-03 10:07:32 +0000

    feat: add python byte array

---
+++ data_types/byte_array.py
@@ -0,0 +1,10 @@
+# Byte array method return an array of the given byte
+
+
+even_numbers = [2, 4, 6, 8]
+
+# convert list to bytearray
+byte_array = bytearray(prime_numbers)
+print(byte_array)
+
+# Output: bytearray(b'\x02\x03\x05\x07')
