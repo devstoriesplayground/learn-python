57217187a7c32d6e389734c0d4c9ad9dc1378e37
CDA <10048477+devstoriesplayground@users.noreply.github.com>
2023-09-07 15:32:49 +0800

    feat: add list, set and tuple data type

---
+++ data_types/set.py
@@ -0,0 +1,13 @@
+# Set is an unordered collection of unique items. Set is defined by values separated by commas inside braces { }
+
+# create a set named student_id
+student_id = {112, 114, 116, 118, 115}
+
+# display student_id elements
+print(student_id)
+
+# display type of student_id
+print(type(student_id))
+
+# {112, 114, 115, 116, 118}
+# <class 'set'>
---
+++ data_types/tuple.py
@@ -0,0 +1,11 @@
+# Tuple is an ordered sequence of items same as a list.
+# The only difference is that tuples are immutable. Tuples once created cannot be modified.
+
+# create a tuple
+product = ('Microsoft', 'Xbox', 499.99)
+
+# access element at index 0
+print(product[0])   # Microsoft
+
+# access element at index 1
+print(product[1])   # Xbox
