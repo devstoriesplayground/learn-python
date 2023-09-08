bdd7cd1dcd473a8974266621290a5fa62c173657
CDA <10048477+devstoriesplayground@users.noreply.github.com>
2023-09-08 16:18:03 +0800

    feat: add dictionary data type; modification

---
+++ data_types/dictionary.py
@@ -0,0 +1,6 @@
+# Python dictionary is an ordered collection of items. It stores elements in key/value pairs.
+
+# create a dictionary named capital_city
+capital_city = {'Nepal': 'Kathmandu', 'Italy': 'Rome', 'England': 'London'}
+
+print(capital_city)
