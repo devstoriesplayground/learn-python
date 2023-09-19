c9a342e1fdb2d2ee7e45d202ea1f16d18de201b4
CDA <10048477+devstoriesplayground@users.noreply.github.com>
2023-09-19 15:52:32 +0800

    feat: add separate file for different python operators

---
+++ operators/arithmetic.py
@@ -0,0 +1,17 @@
+# Arithmetic operators are used to perform mathematical operations like addition, subtraction, multiplication, etc.
+x = 10
+y = 20
+
+z = 0
+z += 3
+
+a = 50
+a &= 25
+
+print(f'addition: {x + y}')
+print(f'subtraction: {y - x}')
+print(f'multiplication: {x * y}')
+print(f'division: {y / x}')
+print(f'modulus: {x % y}')
+print(f'exponentiation: {x ** y}')
+print(f'floor division: {x // y}')
---
+++ operators/logical.py
@@ -0,0 +1,10 @@
+# Logical operators are used to combined conditional statements
+x = 10
+y = 20
+
+if x < 10 and y > 20:
+    print(True)
+else:
+    print(False)
+
+# False
