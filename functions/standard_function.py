82c18e3395efa0bd8d31628b5fe7b9921f0b5a7e
CDA <10048477+devstoriesplayground@users.noreply.github.com>
2023-10-06 17:10:13 +0800

    feat: add standard function in python

---
+++ functions/standard_function.py
@@ -0,0 +1,17 @@
+# Standard library functions - These are built-in functions in Python that are available to use.
+# Example:
+# print() - prints the string inside the quotation marks
+# sqrt() - returns the square root of a number
+# pow() - returns the power of a number
+
+import math
+
+# sqrt computes the square root
+square_root = math.sqrt(4)
+
+print("Square Root of 4 is",square_root)
+
+# pow() comptes the power
+power = pow(2, 3)
+
+print("2 to the power 3 is",power)
